260ff42befc0af5a8b47ec29f83b2d97c641b2a1
jcrr990 <jcrr990@gmail.com>
2014-09-21 15:17:13 -0500

    Se agrego el contenido a la estructura del menu principal

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?
     $opcion = $_GET['opcion'];
+    $controlador = false;
     
     if( $opcion == 'usuario' )
     {
-      
+        require_once 'CONTROLADORES/ControladorUsuario.php';
+        $controlador = new ControladorUsuario;
     }
     else
         if( $opcion == 'vehiculo' )
         {
-            
-        }
-        else
-        {
-        
+            require_once 'CONTROLADORES/ControlVehiculo.php'
+            $controlador = new ControladorVehiculo();
         }
         
-
-
+    
+    if( $controlador )
+        $controlador->ejecutar();
+        
 ?>
