b50e1fc8c6c6c4aef44e356ebea984603a591b82
jcrr990 <jcrr990@gmail.com>
2014-09-21 14:16:39 -0500

    Estructura del menú principal que desplegara todo lo demás :D

---
+++ index.php
@@ -0,0 +1,20 @@
+<?
+    $opcion = $_GET['opcion'];
+    
+    if( $opcion == 'usuario' )
+    {
+      
+    }
+    else
+        if( $opcion == 'vehiculo' )
+        {
+            
+        }
+        else
+        {
+        
+        }
+        
+
+
+?>
